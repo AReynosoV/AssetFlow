4b80a3a13b2fedb1c3dfbcacb71a5bc8b9c4163e
AReynosoV <aneury.reynoso@outlook.com>
2026-01-24 00:17:47 -0400

    feat: add Asset model with data annotations. Closes #2

---
+++ AssetFlow/AssetFlow.App/Models/Asset.cs
@@ -0,0 +1,45 @@
+﻿using DevExpress.XtraPrinting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetFlow.App.Models
+{
+    public class Asset
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Nombre del Activo")]
+        public string Name { get; set; }
+
+
+        [Required]
+        [DisplayName("Categoria")]
+
+        public int CategoryId { get; set; }
+
+        [Required]
+        [DisplayName("Fecha de Compra")]
+        public DateTime PurchaseDate
+        {
+            get; set;
+        }
+
+        [Required]
+        [DisplayName("Estado")]
+        public string Status { get; set; }
+
+        [Required]
+        [DisplayName("Costo")]
+        public decimal Cost { get; set; }
+
+
+    }
+}
